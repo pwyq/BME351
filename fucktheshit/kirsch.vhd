ae207732056445cae0dbaf3e855d11dc2c43a90e
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2019-03-23 14:01:07 -0400

    clean up

--- fucktheshit/kirsch.vhd
+++ fucktheshit/kirsch.vhd
@@ -1,16 +1,13 @@
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 use work.util.all;
---use work.kirsch_synth_pkg.all;
 
 package state_pkg is
-  subtype state_ty is std_logic_vector(2 downto 0);
-  constant S0 : state_ty := "001";
-  constant S1 : state_ty := "010";
-  constant S2 : state_ty := "100";
+	subtype state_ty is std_logic_vector(2 downto 0);
+	constant S0 : state_ty := "001";
+	constant S1 : state_ty := "010";
+	constant S2 : state_ty := "100";
 end state_pkg;
 
 library ieee;
@@ -18,40 +15,36 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.state_pkg.all;
 use work.util.all;
---use work.directions.all;
 use work.kirsch_synth_pkg.all;
 
 entity kirsch is
-  port(
-    clk        : in  std_logic;                      
-    reset      : in  std_logic;                      
-    i_valid    : in  std_logic;                 
-    i_pixel    : in  unsigned(7 downto 0);
-    o_valid    : out std_logic;                 
-    o_edge     : out std_logic;	                     
-    o_dir      : out direction_ty;
-    o_mode     : out mode_ty;
-    o_row      : out unsigned(7 downto 0);
-    o_col      : out unsigned(7 downto 0)
-  );  
+	port(
+		clk        : in  std_logic;                      
+		reset      : in  std_logic;                      
+		i_valid    : in  std_logic;                 
+		i_pixel    : in  unsigned(7 downto 0);
+		o_valid    : out std_logic;                 
+		o_edge     : out std_logic;	                     
+		o_dir      : out direction_ty;
+		o_mode     : out mode_ty;
+		o_row      : out unsigned(7 downto 0);
+		o_col      : out unsigned(7 downto 0)
+	);  
 end entity;
 
 architecture main of kirsch is
 	signal o_mem0 : std_logic_vector(7 downto 0);
 	signal o_mem1 : std_logic_vector(7 downto 0);
 	signal o_mem2 : std_logic_vector(7 downto 0);
-	signal row_state, wr_ens : state_ty;
+	signal row_state : state_ty;
 
 	signal col_counter : unsigned(7 downto 0);
 	signal row_counter : unsigned(7 downto 0);
 
-	signal first_pixel : std_logic_vector(7 downto 0);
-	signal second_pixel : std_logic_vector(7 downto 0);
 	signal a, b, c,
 		   h, i, d,
 		   g, f, e : std_logic_vector(7 downto 0);
 
-	
 	-- stage 1
 	signal reg1, reg2 : std_logic_vector(7 downto 0);
 	-- max1, add1
@@ -71,41 +64,34 @@ architecture main of kirsch is
 	-- sub1
 	-- comp1
 	
-begin  
-  
-  -- wr_ens(0) <= i_valid;
-  -- wr_ens(1) <= i_valid;
-  -- wr_ens(2) <= i_valid;
---   wr_ens(0) <= i_valid and row_state(0) when reset = '0' else '0';
---   wr_ens(1) <= i_valid and row_state(1) when reset = '0' else '0';
---   wr_ens(2) <= i_valid and row_state(2) when reset = '0' else '0';
+begin
 
-  mem_0 : entity work.mem(main)
-  port map(
-      address => col_counter,
-      clock => clk,
-	  data => std_logic_vector(i_pixel), -- input data
-	  wren => i_valid and row_state(0),
-      q => o_mem0						 -- output data
-  );
-  mem_1 : entity work.mem(main)
-  port map(
-      address => col_counter,
-      clock => clk,
-	  data => std_logic_vector(i_pixel),
-	  wren => i_valid and row_state(1),
-      q  => o_mem1
-  );
-  mem_2 : entity work.mem(main)
-  port map(
-      address => col_counter,
-      clock => clk,
-	  data => std_logic_vector(i_pixel),
-	  wren => i_valid and row_state(2),
-      q => o_mem2
-  );
+	mem_0 : entity work.mem(main)
+	port map(
+		address => col_counter,
+		clock => clk,
+		data => std_logic_vector(i_pixel), -- input data
+		wren => i_valid and row_state(0),
+		q => o_mem0						   -- output data
+	);
+	mem_1 : entity work.mem(main)
+	port map(
+		address => col_counter,
+		clock => clk,
+		data => std_logic_vector(i_pixel),
+		wren => i_valid and row_state(1),
+		q  => o_mem1
+	);
+	mem_2 : entity work.mem(main)
+	port map(
+		address => col_counter,
+		clock => clk,
+		data => std_logic_vector(i_pixel),
+		wren => i_valid and row_state(2),
+		q => o_mem2
+	);
 
-  updateState : process begin
+	updateState : process begin
 		wait until rising_edge(clk);
 		if (reset = '1') then
 			-- reset stuff
@@ -131,32 +117,9 @@ begin
 				col_counter <= col_counter + 1;
 			end if;
 		end if;
-  end process;
+	end process;
 
---   setRowPixels : process(o_mem0, o_mem1, o_mem2, row_state) begin
--- 		case row_state is
--- 			when S0 =>
--- 				first_pixel <= o_mem1;
--- 				second_pixel <= o_mem2;
--- 				-- first_pixel <= o_mem1;
--- 				-- second_pixel <= o_mem2;
--- 			when S1 =>
--- 				first_pixel <= o_mem2;
--- 				second_pixel <= o_mem0;
--- 				-- first_pixel <= o_mem2;
--- 				-- second_pixel <= o_mem0;
--- 			when S2 =>
--- 				first_pixel <= o_mem0;
--- 				second_pixel <= o_mem1;
--- 				-- first_pixel <= o_mem0;
--- 				-- second_pixel <= o_mem1;
--- 			when others =>
--- 				first_pixel <= first_pixel;
--- 				second_pixel <= second_pixel;
--- 		end case;
---   end process;
-
-  updateData : process begin
+  updateConvTable : process begin
 		wait until rising_edge(clk);
 		if (i_valid = '1')then
 			-- shift pixel to the left
@@ -166,14 +129,10 @@ begin
 			i <= d;
 			g <= f;
 			f <= e;
-			-- updating col3
-			-- c <= first_pixel;
-			-- d <= second_pixel;
 			c <= std_logic_vector(o_mem0) when row_state(2) else std_logic_vector(o_mem1) when row_state(0) else std_logic_vector(o_mem2);
 			d <= std_logic_vector(o_mem0) when row_state(1) else std_logic_vector(o_mem1) when row_state(2) else std_logic_vector(o_mem2);
 			e <= std_logic_vector(i_pixel);
 		end if;
 	end process;
-	
 
 end architecture;
