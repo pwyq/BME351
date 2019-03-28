769d535a686836e5043e8c145a3dff5b0db14fba
pwyq <y433wu@uwaterloo.ca>
2019-03-27 22:57:15 -0400

    CAPs and shorten some code

--- kirsch_proj/kirsch.vhd
+++ kirsch_proj/kirsch.vhd
@@ -1,113 +1,114 @@
-library IEEE;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY IEEE;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
-package state_pkg is
-	subtype state_ty is std_logic_vector(2 downto 0);
-		constant S0 : state_ty := "001";
-		constant S1 : state_ty := "010";
-		constant S2 : state_ty := "100";
+PACKAGE state_pkg IS
+	SUBTYPE state_ty IS STD_LOGIC_VECTOR(2 DOWNTO 0);
+		CONSTANT S0 : state_ty := "001";
+		CONSTANT S1 : state_ty := "010";
+		CONSTANT S2 : state_ty := "100";
 
 	----------- Structures -----------
-	subtype o_mem_data is std_logic_vector(7 downto 0); 
-		type o_mem_vec is array(natural range <>) of o_mem_data;
+	SUBTYPE o_mem_data IS STD_LOGIC_VECTOR(7 DOWNTO 0); 
+		TYPE o_mem_vec IS ARRAY(NATURAL RANGE <>) OF o_mem_data;
 
-	type maxOneStruct is RECORD
-		val : std_logic_vector(7 downto 0);
-		dir : std_logic_vector(2 downto 0);
-	end RECORD;
+	TYPE maxOneStruct IS RECORD
+		val : STD_LOGIC_VECTOR(7 DOWNTO 0);
+		dir : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	END RECORD;
 
-	type maxTwoStruct is RECORD
-		val : unsigned(12 downto 0);
-		dir : std_logic_vector(2 downto 0);
-	end RECORD;
+	TYPE maxTwoStruct IS RECORD
+		val : UNSIGNED(12 DOWNTO 0);
+		dir : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	END RECORD;
   
-end state_pkg;
+END state_pkg;
 
-library IEEE;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.state_pkg.all;
-use work.util.all;
-use work.kirsch_synth_pkg.all;
+LIBRARY IEEE;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE work.state_pkg.all;
+USE work.util.all;
+USE work.kirsch_synth_pkg.all;
 
-entity kirsch is
-	port(
-		clk        : in  std_logic;				-- clock                      
-		reset      : in  std_logic;    			-- reset signal                  
-		i_valid    : in  std_logic;          	-- is input valid?       
-		i_pixel    : in  unsigned(7 downto 0);	-- 8-bit input
-		o_valid    : out std_logic;    			-- is output valid?             
-		o_edge     : out std_logic;				-- 1-bit output for edge	                     
-		o_dir      : out direction_ty;			-- 3-bit output for direction
-		o_mode     : out mode_ty;				-- 2-bit output for mode; (busy|idle|reset; see kirsch_synth_pkg.vhd)
-		o_row      : out unsigned(7 downto 0);	-- row number of the input image
-		o_col      : out unsigned(7 downto 0)	-- col number of the input image
+ENTITY kirsch IS
+	PORT(
+		clk        : IN  STD_LOGIC;				-- clock                      
+		reset      : IN  STD_LOGIC;    			-- reset SIGNAL                  
+		i_valid    : IN  STD_LOGIC;          	-- is input valid?       
+		i_pixel    : IN  UNSIGNED(7 DOWNTO 0);	-- 8-bit input
+		o_valid    : OUT STD_LOGIC;    			-- is output valid?             
+		o_edge     : OUT STD_LOGIC;				-- 1-bit output for edge	                     
+		o_dir      : OUT direction_ty;			-- 3-bit output for direction
+		o_mode     : OUT mode_ty;				-- 2-bit output for mode; (busy|idle|reset; see kirsch_synth_pkg.vhd)
+		o_row      : OUT UNSIGNED(7 DOWNTO 0);	-- row number of the input image
+		o_col      : OUT UNSIGNED(7 DOWNTO 0)	-- col number of the input image
 	);  
-end entity;
+END ENTITY;
 
-architecture main of kirsch is
+ARCHITECTURE main of kirsch IS
 	----------- Functions -----------
-	function maxOperator1(p1_val, p1_dir, p2_val, p2_dir : std_logic_vector)
-	return maxOneStruct is variable pMax : maxOneStruct;
-	begin
-		if (p1_val >= p2_val) then
+	FUNCTION maxOperator1(p1_val, p1_dir, p2_val, p2_dir : STD_LOGIC_VECTOR)
+	RETURN maxOneStruct IS VARIABLE pMax : maxOneStruct;
+	BEGIN
+		IF (p1_val >= p2_val) THEN
 			pMax.val := p1_val;
 			pMax.dir := p1_dir;
-		else
+		ELSE
 			pMax.val := p2_val;
 			pMax.dir := p2_dir;
-		end if;
-		return pMax;
-	end maxOperator1;
+		END IF;
+		RETURN pMax;
+	END maxOperator1;
 
-	function maxOperator2(p1_val : unsigned; p1_dir : std_logic_vector;
-						  p2_val : unsigned; p2_dir : std_logic_vector)
-	return maxTwoStruct is variable pMax : maxTwoStruct;
-	begin
-		if (p1_val >= p2_val) then
+	FUNCTION maxOperator2(p1_val : UNSIGNED; p1_dir : STD_LOGIC_VECTOR;
+						  p2_val : UNSIGNED; p2_dir : STD_LOGIC_VECTOR)
+	RETURN maxTwoStruct IS VARIABLE pMax : maxTwoStruct;
+	BEGIN
+		IF (p1_val >= p2_val) THEN
 			pMax.val := p1_val;
 			pMax.dir := p1_dir;
-		else
+		ELSE
 			pMax.val := p2_val;
 			pMax.dir := p2_dir;
-		end if;
-		return pMax;
-	end maxOperator2;
+		END IF;
+		RETURN pMax;
+	END maxOperator2;
 	
 	-------- General Signals --------
-	signal a, b, c,
+	SIGNAL a, b, c,
 		   h, i, d,
-		   g, f, e : std_logic_vector(7 downto 0);
-	signal col_counter, row_counter : unsigned(7 downto 0);
-	signal o_mems                   : o_mem_vec(2 downto 0);
-	signal row_state                : state_ty;
-	signal cycles 					: std_logic_vector(7 downto 0);
-	signal isComputable				: std_logic;
-	signal on_stage1, on_stage2, on_stage3 : std_logic;
+		   g, f, e : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL col_counter  : UNSIGNED(7 DOWNTO 0);
+	SIGNAL row_counter  : UNSIGNED(7 DOWNTO 0);
+	SIGNAL o_mems       : o_mem_vec(2 DOWNTO 0);
+	SIGNAL row_state    : state_ty;
+	SIGNAL cycles       : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL isComputable : STD_LOGIC;
+	SIGNAL on_stage1, on_stage2, on_stage3 : STD_LOGIC;
 	
 	-------- Stage 1 Signals --------
-	signal add1 : unsigned(8 downto 0);
-	signal max1 : maxOneStruct;
-	signal add2 : unsigned(9 downto 0);
-	signal reg1 : maxTwoStruct;
-	signal reg2 : unsigned(8 downto 0); 
+	SIGNAL add1 : UNSIGNED(8 DOWNTO 0);
+	SIGNAL max1 : maxOneStruct;
+	SIGNAL add2 : UNSIGNED(9 DOWNTO 0);
+	SIGNAL reg1 : maxTwoStruct;
+	SIGNAL reg2 : UNSIGNED(8 DOWNTO 0); 
 	
 	-------- Stage 2 Signals --------
-	signal add3 : unsigned(13 downto 0);
-	signal reg3 : unsigned(13 downto 0);
+	SIGNAL add3 : UNSIGNED(13 DOWNTO 0);
+	SIGNAL reg3 : UNSIGNED(13 DOWNTO 0);
 	
 	-------- Stage 3 Signals --------
-	signal reg4 : maxTwoStruct;
-	signal max2 : maxTwoStruct;
+	SIGNAL reg4 : maxTwoStruct;
+	SIGNAL max2 : maxTwoStruct;
 	
 	-------- Stage 4 Signals --------
-	signal sub1 : signed(13 downto 0);
-	signal reg5 : signed(13 downto 0);
-	signal validEdge : std_logic;
-	signal resultDir : std_logic_vector(2 downto 0);
-	signal edgeFound : std_logic;
-begin  
+	SIGNAL sub1 : SIGNED(13 DOWNTO 0);
+	SIGNAL reg5 : SIGNED(13 DOWNTO 0);
+	SIGNAL validEdge : STD_LOGIC;
+	SIGNAL resultDir : direction_ty;
+	SIGNAL edgeFound : STD_LOGIC;
+BEGIN  
 -- --------------------------------
 -- Preprocessing
 -- --------------------------------
@@ -116,162 +117,166 @@ begin
 	o_mode(1) <= NOT reset;
 	o_mode(0) <= reset OR on_stage1 OR on_stage2 OR on_stage3;
 
-	isComputable <= '1' when (row_counter >= to_unsigned(2, 8) AND col_counter >= to_unsigned(2, 8)) else '0';
+	isComputable <= '1' WHEN (row_counter >= 2 AND col_counter >= 2) ELSE '0';
 
-	MEMS : for i in 0 to 2 generate 
-		mem : entity work.mem(main)
-		port map (
+	MEMS : FOR i IN 0 TO 2 GENERATE 
+		mem : ENTITY work.mem(main)
+		PORT MAP (
 			address => col_counter,
 			clock   => clk,
-			data    => std_logic_vector(i_pixel),
+			data    => STD_LOGIC_VECTOR(i_pixel),
 			wren    => row_state(i),
 			q       => o_mems(i)
 		);
-	end generate MEMS;
+	END GENERATE MEMS;
 
-	updateState : process begin 
-		wait until rising_edge(clk);
-		if (reset = '1') then
+	updateState : PROCESS BEGIN 
+		WAIT UNTIL RISING_EDGE(clk);
+		IF reset THEN
 			row_state <= S0;
 			col_counter <= to_unsigned(0, 8);
 			row_counter <= to_unsigned(0, 8);
-		elsif (i_valid = '1') then
+		ELSIF i_valid THEN
 			on_stage1 <= '1';			 
-			if (col_counter = to_unsigned(255, 8)) then
+			IF (col_counter = 255) THEN
 				row_counter <= row_counter + 1;
-				if (row_counter = to_unsigned(255, 8)) then 
+				IF (row_counter = 255) THEN 
 					row_state <= S0;
 					on_stage1 <= '0';
-				else
-					case (row_state) is
-						when S0 =>
+				ELSE
+					case (row_state) IS
+						WHEN S0 =>
 							row_state <= S1;
-						when S1 =>
+						WHEN S1 =>
 							row_state <= S2;
-						when S2 =>
+						WHEN S2 =>
 							row_state <= S0;
-						when others =>
+						WHEN others =>
 							row_state <= row_state;
-					end case;
-				end if;
-			end if;
+					END case;
+				END IF;
+			END IF;
 			col_counter <= col_counter + 1;
-		end if;
-	end process;
+		END IF;
+	END PROCESS;
 
 -- --------------------------------
 -- Convolution Table
 -- --------------------------------
-	updateConvTable : process begin
-		wait until rising_edge(clk);
-		if (i_valid = '1') then
+	updateConvTable : PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);
+		IF i_valid THEN
 			a <= b;
 			b <= c;
-			c <= std_logic_vector(o_mems(0)) when row_state(2) else std_logic_vector(o_mems(1)) when row_state(0) else std_logic_vector(o_mems(2));
+			c <= STD_LOGIC_VECTOR(o_mems(0)) WHEN row_state(2) ELSE STD_LOGIC_VECTOR(o_mems(1)) WHEN row_state(0) ELSE STD_LOGIC_VECTOR(o_mems(2));
 			h <= i;
 			i <= d;
-			d <= std_logic_vector(o_mems(0)) when row_state(1) else std_logic_vector(o_mems(1)) when row_state(2) else std_logic_vector(o_mems(2));
+			d <= STD_LOGIC_VECTOR(o_mems(0)) WHEN row_state(1) ELSE STD_LOGIC_VECTOR(o_mems(1)) WHEN row_state(2) ELSE STD_LOGIC_VECTOR(o_mems(2));
 			g <= f;
 			f <= e;
-			e <= std_logic_vector(i_pixel);
-		end if;
-	end process;
+			e <= STD_LOGIC_VECTOR(i_pixel);
+		END IF;
+	END PROCESS;
 
-	updateCycles : process begin 
-		wait until rising_edge(clk);
-		if(reset = '1') then
-			cycles(7 downto 0) <= "00000000";
-		else
+	updateCycles : PROCESS BEGIN 
+		WAIT UNTIL RISING_EDGE(clk);
+		IF reset THEN
+			cycles(7 DOWNTO 0) <= "00000000";
+		ELSE
 			cycles(0) <= (isComputable AND i_valid);
-			cycles(7 downto 1) <= cycles(6 downto 0); 
-		end if;
-	end process;
+			cycles(7 DOWNTO 1) <= cycles(6 DOWNTO 0); 
+		END IF;
+	END PROCESS;
 		  		  
 -- --------------------------------
 -- Stage 1
 -- --------------------------------
-	add1 <= resize(unsigned(a), 9) + resize(unsigned(h), 9) when cycles(0) else
-			resize(unsigned(b), 9) + resize(unsigned(c), 9) when cycles(1) else
-			resize(unsigned(d), 9) + resize(unsigned(e), 9) when cycles(2) else
-			resize(unsigned(f), 9) + resize(unsigned(g), 9) when cycles(3) else
+	add1 <= resize(UNSIGNED(a), 9) + resize(UNSIGNED(h), 9) WHEN cycles(0) ELSE
+			resize(UNSIGNED(b), 9) + resize(UNSIGNED(c), 9) WHEN cycles(1) ELSE
+			resize(UNSIGNED(d), 9) + resize(UNSIGNED(e), 9) WHEN cycles(2) ELSE
+			resize(UNSIGNED(f), 9) + resize(UNSIGNED(g), 9) WHEN cycles(3) ELSE
 			to_unsigned(0, 9); 
 					
-	max1 <= maxOperator1(g, dir_w, b, dir_nw) when cycles(0) else
-			maxOperator1(a, dir_n, d, dir_ne) when cycles(1) else
-			maxOperator1(c, dir_e, f, dir_se) when cycles(2) else
-			maxOperator1(e, dir_s, h, dir_sw) when cycles(3) else
+	max1 <= maxOperator1(g, dir_w, b, dir_nw) WHEN cycles(0) ELSE
+			maxOperator1(a, dir_n, d, dir_ne) WHEN cycles(1) ELSE
+			maxOperator1(c, dir_e, f, dir_se) WHEN cycles(2) ELSE
+			maxOperator1(e, dir_s, h, dir_sw) WHEN cycles(3) ELSE
 			maxOperator1("00000000", "000", "00000000", "000"); 
 
-	add2 <= resize(add1, 10) + resize(unsigned(max1.val), 10);
+	add2 <= resize(add1, 10) + resize(UNSIGNED(max1.val), 10);
 
-	updateStageOne : process begin
-		wait until rising_edge(clk);
+	updateStageOne : PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);
 		reg1.val <= resize(add2, 13);
 		reg1.dir <= max1.dir;
-	end process;
+	END PROCESS;
 
 -- --------------------------------
 -- Stage 2
 -- --------------------------------
-	add3 <= resize(reg2, 14) + resize(add1, 14) when cycles(1) = '1' else 
-			resize(reg3, 14) + resize(SHIFT_LEFT(reg3, 1), 14) when cycles(4) = '1' else
+	add3 <= resize(reg2, 14) + resize(add1, 14) WHEN cycles(1) ELSE 
+			resize(reg3, 14) + resize(SHIFT_LEFT(reg3, 1), 14) WHEN cycles(4) ELSE
 			resize(add1, 14) + resize(reg3, 14);
 				
-	updateStageTwo: process begin
-		wait until rising_edge(clk);
-		if (cycles(0)= '1') then
+	updateReg2: PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);
+		IF cycles(0) THEN
 			reg2 <= add1;	
-		end if;
+		END IF;
+	END PROCESS;
+
+	updateReg3 : PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);
 		on_stage2 <= on_stage1;
 		reg3 <= add3; 
-	end process;
+	END PROCESS;
 
 -- --------------------------------
 -- Stage 3
 -- --------------------------------
 	max2 <= maxOperator2(reg4.val, reg4.dir, reg1.val, reg1.dir);
 
-	updateStageThree : process begin   
-		wait until rising_edge(clk);	
-			if (cycles(1) = '1') then
+	updateStageThree : PROCESS BEGIN   
+		WAIT UNTIL RISING_EDGE(clk);	
+			IF cycles(1) THEN
 				reg4.val <= resize(reg1.val, 13);
 				reg4.dir <= reg1.dir;			
-			elsif (cycles(4) = '1') then
+			ELSIF cycles(4) THEN
 				reg4.val <= SHIFT_LEFT(max2.val, 3);
 				reg4.dir <= max2.dir;
 				resultDir <= max2.dir;
-			else
+			ELSE
 				reg4.val <= max2.val; 
 				reg4.dir <= max2.dir;
-			end if; 
+			END IF; 
 			
 			on_stage3 <= on_stage2;
-	end process;
+	END PROCESS;
 
 -- --------------------------------
 -- Stage 4
 -- --------------------------------
-	sub1 <= signed(resize(reg4.val, 14)) - signed(resize(reg3, 14));
-	edgeFound <= '1' when (reg5 > 383) else '0';
+	sub1 <= SIGNED(resize(reg4.val, 14)) - SIGNED(resize(reg3, 14));
+	edgeFound <= '1' WHEN (reg5 > 383) ELSE '0';
 
-	updateResult : process begin
-		wait until rising_edge(clk);	 
-		if (cycles(5) = '1') then
+	updateResult : PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);	 
+		IF cycles(5) THEN
 			reg5 <= sub1;
-		end if;
-	end process;
+		END IF;
+	END PROCESS;
 
-	updateOutput : process begin
-		wait until rising_edge(clk);
-		if cycles(6) = '1' then
+	updateOutput : PROCESS BEGIN
+		WAIT UNTIL RISING_EDGE(clk);
+		IF cycles(6) THEN
 			validEdge <= edgeFound;
-		else
+		ELSE
 			validEdge <= '0';
-		end if;
-	end process;
+		END IF;
+	END PROCESS;
 
 	o_edge  <= validEdge;
-	o_dir   <= resultDir when (validEdge = '1') else "000";
+	o_dir   <= resultDir WHEN validEdge ELSE "000";
 	o_valid <= cycles(7);
 
-end architecture;
+END ARCHITECTURE;
